3df4b50e35b9eb62d9ff4611f5b08bc192e13f36
Jerrold Pancho <jerpancho@gmail.com>
2021-09-22 19:56:35 -0500

    details page

--- lib/views/details.dart
+++ lib/views/details.dart
@@ -8,8 +8,52 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
+  //variables to be replaced by database implementation
+  String title = "Gimlet";
+  String imgUrl =
+      "https://www.thecocktaildb.com/images/media/drink/3xgldt1513707271.jpg";
+  List<String> ingredients = ["Gin", "Lime Juice", "Sugar Syrup", "Lime"];
+  //
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xFFE8DFDA),
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back)),
+        title: Row(
+          children: [
+            const Expanded(child: Text("Test")),
+            Expanded(
+                child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.star_border),
+            ))
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill, image: NetworkImage(imgUrl))),
+          )),
+          Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Container(
+                    color: Colors.blue,
+                    child: Column(
+                      children: [Text("data")],
+                    ),
+                  )))
+        ],
+      ),
+    );
   }
 }
