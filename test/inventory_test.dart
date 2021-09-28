ae210554b878e6aee8a78f8412c262443b7018b6
Jerrold Pancho <jerpancho@gmail.com>
2021-09-28 18:53:20 -0500

    added measurement unit test

--- test/inventory_test.dart
+++ test/inventory_test.dart
@@ -15,8 +15,11 @@ void main() {
       //when making an instance of an inventory expect the amount to be the instance given.
       expect(inventory.getIngredient, 'orange');
     });
+    test('Test inventory with a given measurement', () async {
+      final inventory = MyInventory('orange', 5, 'oz');
 
-    //if im given an ingredient the class should figure out what the unit of measurement
-    //is for the amount.
+      //when making an instance of an inventory expect the amount to be the instance given.
+      expect(inventory.getMeasurement, 'oz');
+    });
   });
 }
