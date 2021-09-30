2ce65b9c16b86cfacdb394eb9ef22f8e0bd5805f
Jerrold Pancho <jerpancho@gmail.com>
2021-09-30 09:21:34 -0500

    added search widget testing

---
+++ test/searchwidget_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:app/views/search.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  group("test search bar widget", () {});
+}
