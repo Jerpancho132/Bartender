28f299b9967308a5a11dc0b6ba1fa1e1cf0c3ca6
Jerrold Pancho <jerpancho@gmail.com>
2021-11-02 12:35:49 -0500

    created cocktail model

--- lib/models/cocktail.dart
+++ lib/models/cocktail.dart
@@ -1,7 +1,23 @@
 class Cocktail {
-  String name;
-  List ingredients;
-  String instruction;
-  String picture;
-  Cocktail(this.name, this.ingredients, this.instruction, this.picture);
+  final int id;
+  final String title;
+  final String image;
+  final String glassType;
+  final String instruction;
+
+  Cocktail(
+      {required this.id,
+      required this.title,
+      required this.image,
+      required this.glassType,
+      required this.instruction});
+
+  factory Cocktail.fromJson(Map<String, dynamic> json) {
+    return Cocktail(
+        id: json['id'],
+        title: json['title'],
+        image: json['image'],
+        glassType: json['glasstype'],
+        instruction: json['instruction']);
+  }
 }
