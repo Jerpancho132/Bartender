253b8b9ee2d6dc4c329244c9c05120404d773b74
Jerrold Pancho <jerpancho@gmail.com>
2021-11-07 14:33:39 -0600

    added filter buttons for ingredients

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -2,9 +2,11 @@ import 'dart:convert';
 import 'package:app/views/favorites.dart';
 import 'package:app/views/inventory.dart';
 import 'package:app/views/results.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:app/views/home.dart';
 import 'package:app/models/cocktail.dart';
+import 'package:flutter/painting.dart';
 import 'package:http/http.dart' as http;
 
 class SearchPage extends StatefulWidget {
@@ -111,7 +113,7 @@ class _SearchPageState extends State<SearchPage> {
     setIngredients();
   }
 
-  //list of every possible ingredients in the database;
+  //list of every possible ingredients in the database initialized
   List ingredients = [];
 
   //added list of ingredients to filter by;
@@ -137,46 +139,70 @@ class _SearchPageState extends State<SearchPage> {
         ),
         body: Column(
           children: [
-            Container(
-              margin: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black),
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-              ),
-              child: TextField(
-                  key: const Key("searchbar"),
-                  //this is the search bar that will filter out the dataset
-                  controller: _controller,
-                  decoration: const InputDecoration(
-                      hintText: "Search for Cocktail",
-                      contentPadding: EdgeInsets.only(left: 10)),
-                  onChanged: searchFunction),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white),
+                    child: TextField(
+                        key: const Key("searchbar"),
+                        //this is the search bar that will filter out the dataset
+                        controller: _controller,
+                        decoration: const InputDecoration(
+                            hintText: "Search for Cocktail",
+                            contentPadding: EdgeInsets.only(left: 10)),
+                        onChanged: searchFunction),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      //initialize a new list that first takes in the search list
+                      List<Cocktail> filter = searchList;
+                      //Filter by given ingredients
+                      filter = await searchbyIngredients(_ingredientsFilter);
+                      //condition if the textfield has an input
+                      //or if the filter buttons have been pressed
+                      if (filter.isNotEmpty) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Results(result: filter)));
+                      } else {
+                        print('list is empty');
+                      }
+                    },
+                    child: const Text('Search')),
+                const Padding(padding: EdgeInsets.only(right: 10))
+              ],
             ),
-            //should create a grid view here that builds the
-            //list but shows nothing if the api call gets nothing.
-            ElevatedButton(
-                onPressed: () async {
-                  searchList = await searchbyIngredients(['Gin', 'Lime']);
-                  //condition if the textfield has an input
-                  //or if the filter buttons have been pressed
-                  if (_controller.text.isNotEmpty ||
-                      _ingredientsFilter.isNotEmpty) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Results(result: searchList)));
-                  } else {
-                    print('list is empty');
-                  }
-                },
-                child: const Text('Search')),
+            const Center(
+                child: Text(
+              'Filters',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            )),
+            const Padding(padding: EdgeInsets.only(top: 10)),
             //set up a list view for each type of filter categories
             //first category is ingredients, then age-range, then location
             ListView(
               shrinkWrap: true,
               children: [
                 //setup a grid view of ingredient options
+                const Center(
+                    child: Text(
+                  'Ingredients',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                )),
+                //should create a grid view here that builds the
+                //list of ingredients but shows nothing if the api call gets nothing.
+                const Padding(padding: EdgeInsets.only(top: 10)),
                 ingredients.isNotEmpty
                     ? GridView.builder(
                         shrinkWrap: true,
@@ -190,10 +216,10 @@ class _SearchPageState extends State<SearchPage> {
                         itemBuilder: (BuildContext c, index) {
                           //replace this with buttons that will add
                           //them to the filter list
-                          return Text(ingredients[index]);
+                          return ingredientCard(ingredients[index]);
                         })
                     : const Center(
-                        child: Text('No Ingredients'),
+                        child: Text('No ingredients data found'),
                       ),
                 //setup next grid view for next category
               ],
@@ -232,10 +258,43 @@ class _SearchPageState extends State<SearchPage> {
     );
   }
 
-  //searches for the
+  //filter button widget for each ingredient
+  Widget ingredientCard(String i) => GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          setState(() {
+            //if clicked add to filters list
+            if (!_ingredientsFilter.contains(i)) {
+              _ingredientsFilter.add(i);
+            } else {
+              //else remove from filters list
+              _ingredientsFilter.remove(i);
+            }
+            print(_ingredientsFilter);
+            print(searchList);
+          });
+        },
+        child: Container(
+            padding: const EdgeInsets.all(5),
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: _ingredientsFilter.contains(i)
+                  ? Colors.red.shade700
+                  : Colors.red.shade300,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(
+                i,
+                style: const TextStyle(fontSize: 15),
+              ),
+            )),
+      );
+  //searches for the list of cocktail by input string
   void searchFunction(String s) {
     //starts the searchList with the full list
-    print(ingredients);
+    print(searchList.map((e) => e.title).toList());
     searchList = cocktailList;
 
     final filteredSearch = searchList.where((cocktail) {
@@ -258,7 +317,7 @@ class _SearchPageState extends State<SearchPage> {
         }
         //makes ids distinct
         ids = ids.toSet().toList();
-        //filters the searchlist which is already filtered by
+        //filters the searchlist which has already been filtered by
         //searchFunction and further filters it by ingredients
         final _filteredSearch = searchList.where((cocktail) {
           return ids.contains(cocktail.id);
