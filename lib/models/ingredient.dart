5eaa655d49148314ce4838812abd212316a6d071
Jerrold Pancho <jerpancho@gmail.com>
2021-11-28 09:49:51 -0600

    added ingredients model

---
+++ lib/models/ingredient.dart
@@ -0,0 +1,23 @@
+class Ingredient {
+  final int id;
+  final String title;
+  final String measurement;
+  final String image;
+  final String description;
+
+  Ingredient(
+      {required this.id,
+      required this.title,
+      required this.measurement,
+      this.image = "",
+      this.description = ""});
+
+  factory Ingredient.fromJson(Map<String, dynamic> json) {
+    return Ingredient(
+        id: json["id"],
+        title: json["title"],
+        measurement: json["measurement"],
+        image: json["image"],
+        description: json["description"]);
+  }
+}
