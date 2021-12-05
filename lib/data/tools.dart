38113eb7a3da0dba74c38093ea6a247e700e2b04
Jerrold Pancho <jerpancho@gmail.com>
2021-12-05 11:47:04 -0600

    added tools data

---
+++ lib/data/tools.dart
@@ -0,0 +1,12 @@
+import 'package:app/models/description.dart';
+
+List<Description> tools = [
+  Description(
+      name: "Shaker",
+      description:
+          "A cocktail shaker is a device used to mix beverages (usually alcoholic) by shaking. When ice is put in the shaker this allows for a quicker cooling of the drink before serving."),
+  Description(
+      name: "Strainer",
+      description:
+          "A Cocktail strainer is a bartending tool used to remove solid materials from the liquid in the making of a drink. Also known as a bar strainer, the cocktail strainer is typically used after a drink has been shaken or stirred to remove the ice, fruit or other solids that shouldn’t find their way into the resulting cocktail. There are different types such as the Hawthorne, Julep, and fine mesh strainer.")
+];
