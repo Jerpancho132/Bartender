83dc7c8c2f272fdc044d4585119a1ef2454ef46d
Jerrold Pancho <jerpancho@gmail.com>
2021-12-05 11:41:53 -0600

    created data folder and description model

---
+++ lib/models/description.dart
@@ -0,0 +1,10 @@
+class Description {
+  final String name;
+  final String description;
+
+  Description({required this.name, required this.description});
+
+  String get getName => name;
+
+  String get getDescription => description;
+}
